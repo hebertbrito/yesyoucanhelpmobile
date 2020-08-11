e16954bc8ca4eb21e6a11e9f247ceefc5c4c839a
hebert felipe <hebertfelipe.97@outlook.com.br>
2020-08-11 13:25:36 -0300

    created new changes
    button drawer to open drawer navigation

---
+++ src/components/buttonDrawer/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { IconButton, Text, useTheme } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useNavigation } from '@react-navigation/native';
+import { DrawerContentComponentProps, DrawerContentOptions } from '@react-navigation/drawer';
+
+
+
+
+interface Props {
+    navigate?: any,
+    width?: string,
+    height?: number,
+    iconName?: string,
+    screenNameNavigate?: string
+}
+
+export const ButtonDrawer = (props: Props) => {
+
+    const navigation = useNavigation<DrawerContentComponentProps<any>>();
+
+    const iconButton = () => {
+        return (
+            <Icon name="bars" size={20} color={paperTheme.colors.onBackground} />
+        )
+    }
+
+    const paperTheme = useTheme();
+
+    return (
+        <IconButton
+            icon={iconButton}
+            onPress={() => navigation.openDrawer()}
+            style={{position: "absolute", margin: 5, zIndex: +1}}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
--- src/pages/homescreen/index.tsx
+++ src/pages/homescreen/index.tsx
@@ -9,16 +9,16 @@ import { useNavigation } from '@react-navigation/native';
 //componentes
 import { ProgressBarComponent } from '../../components/progressbar'
 import { ButtonCommum } from '../../components/buttonCommum'
+import { ButtonDrawer } from '../../components/buttonDrawer'
 
 const HomeScreen = ({ ...props }) => {
     const paperTheme = useTheme()
     const { navigate } = useNavigation()
     return (
         <SafeAreaView style={styles.safeView}>
-
             <Animatable.Image animation="fadeInDown" delay={500} useNativeDriver={true} source={require('../../assets/fotospublic/social.jpg')} style={{ height: '62%', width: '100%', position: "absolute" }}
                 borderBottomLeftRadius={20} borderBottomRightRadius={20} />
-
+            <ButtonDrawer navigate={navigate} />
             <Animatable.View animation="slideInUp" delay={500} useNativeDriver={true} style={styles.viewInfo}>
                 <View style={{marginBottom: 20, marginTop: 30}}>
                     <ProgressBarComponent title="Better Profile" value={6} />
